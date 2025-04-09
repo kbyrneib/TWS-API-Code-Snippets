2f53d7fe520b3ffab6dfe3fb6b478cf15c87c1b1
Keelan Byrne <kbyrne@interactivebrokers.co.uk>
2025-04-09 15:07:36 +0100

    add simple example of requesting news bulletins from IB

---
+++ Bulletins/news_bulletins.py
@@ -0,0 +1,38 @@
+from ibapi.client import *
+from ibapi.wrapper import *
+
+class TestApp(EClient, EWrapper):
+    def __init__(self):
+        EClient.__init__(self, wrapper=self)
+
+    def nextValidId(self, orderId):
+        print(f"Next valid order ID is {orderId}")
+        self.orderId = orderId
+        self.start()
+
+    def nextId(self):
+        self.orderId += 1
+        return self.orderId
+
+    def start(self):
+        # Subscribe to IB's News Bulletins
+        self.reqNewsBulletins(True)
+
+    def updateNewsBulletin(self, msgId, msgType, newsMessage, originExch):
+        print(msgId, msgType, newsMessage, originExch)
+
+        # Cancel IB's news bulletin subscription
+        self.cancelNewsBulletins()
+
+    def error(self, reqId, errorTime, errorCode, errorString, advancedOrderRejectJson=""):
+        print(errorCode, errorString)
+
+# Required args for connectivity
+host = "localhost"
+port = 7497
+clientId = 0
+
+# Creating, connecting and running app
+app = TestApp()
+app.connect(host, port, clientId)
+app.run()
