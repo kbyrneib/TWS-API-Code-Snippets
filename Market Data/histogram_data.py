34878823ecd97f4f77aac6a3e45dba800cf6fe46
Keelan Byrne <kbyrne@interactivebrokers.co.uk>
2025-04-10 16:34:59 +0100

    add example of requesting data histogram for a contract

---
+++ Market Data/histogram_data.py
@@ -0,0 +1,45 @@
+from ibapi.client import *
+from ibapi.wrapper import *
+import datetime
+
+class TestApp(EClient, EWrapper):
+    def __init__(self):
+        EClient.__init__(self, wrapper=self)
+
+
+
+    def nextValidId(self, orderId):
+        print(f"Next valid order ID is {orderId}")
+        self.orderId = orderId
+        self.start()
+
+    def nextId(self):
+        self.orderId += 1
+        return self.orderId
+
+    def start(self):
+        # Building a contract
+        contract = Contract()
+        contract.symbol = "IBM"
+        contract.secType = "STK"
+        contract.exchange = "SMART"
+        contract.currency = "USD"
+
+        # Requesting data histogram of specified contract
+        self.reqHistogramData(self.nextId(), contract, False, "3 days")
+
+    def histogramData(self, reqId, items):
+        print(reqId, items)
+
+    def error(self, reqId, errorTime, errorCode, errorString, advancedOrderRejectJson=""):
+        print(errorCode, errorString)
+
+# Required args for connectivity
+host = "localhost"
+port = 7497
+clientId = 0
+
+# Creating, connecting and running app
+app = TestApp()
+app.connect(host, port, clientId)
+app.run()
