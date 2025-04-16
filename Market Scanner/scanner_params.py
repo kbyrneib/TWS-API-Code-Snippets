90f94223bbea317d3db3f34c7f686977dc030136
Keelan Byrne <kbyrne@interactivebrokers.co.uk>
2025-04-16 11:03:08 +0100

    add example requesting scanner xml parameters and saving to a file

---
+++ Market Scanner/scanner_params.py
@@ -0,0 +1,37 @@
+from ibapi.client import *
+from ibapi.wrapper import *
+
+class TestApp(EClient, EWrapper):
+    def __init__(self):
+        EClient.__init__(self, wrapper=self)
+
+    def nextValidId(self, orderId):
+        print(f"Next valid order ID is {orderId}")
+        self.orderId = orderId
+        self.start()
+
+    def nextId(self):
+        self.orderId += 1
+        return self.orderId
+
+    def start(self):
+        # Requesting an XML list of scanner params that are valid in TWS
+        self.reqScannerParameters()
+
+    def scannerParameters(self, xml):
+        # Save scanner params to a file
+        with open("scanner.xml", "w") as f:
+            f.write(xml)
+
+    def error(self, reqId, errorTime, errorCode, errorString, advancedOrderRejectJson=""):
+        print(errorCode, errorString)
+
+# Required args for connectivity
+host = "localhost"
+port = 7497
+clientId = 0
+
+# Creating, connecting and running app
+app = TestApp()
+app.connect(host, port, clientId)
+app.run()
