24f08b6fd38c47d83fdc4366da2233937a936df7
Keelan Byrne <kbyrne@interactivebrokers.co.uk>
2025-06-12 11:43:45 +0100

    update stop order

--- Python/Orders/stop.py
+++ Python/Orders/stop.py
@@ -20,18 +20,25 @@ class TestApp(EClient, EWrapper):
         contract.conId = 265598 # AAPL
         contract.exchange = "SMART"
         
-        # Build Order
+        # Order Type: Stop Order
+        # Explanation: Submit a market order if stop price is attained/penetrated. May be simulated before becoming marketable.
+        # General Link: https://www.interactivebrokers.co.uk/en/trading/ordertypes.php?m=stopModal
+        # API Link: https://www.interactivebrokers.com/campus/ibkr-api-page/order-types/#stop-order
+        # Building the Order with required attributes
         order = Order()
-        order.action = "SELL"
-        order.orderType = "STP"
-        order.totalQuantity = 1
-        order.auxPrice = 206.0
-
+        order.action = "BUY" # Identifies the side, BUY in this case
+        order.orderType = "STP" # The order type (obviously)
+        order.totalQuantity = 1 # The number of positions being bought/sold (Qty in TWS GUI)
+        order.auxPrice = 198.0 # The stop price - if attained/penetrated, a market order is submitted
+        
         # Place Order
         self.placeOrder(self.nextId(), contract, order)
 
     def orderStatus(self, orderId, status, filled, remaining, avgFillPrice, permId, parentId, lastFillPrice, clientId, whyHeld, mktCapPrice):
         print(f"OrderStatus:, orderId: {orderId}, status: {status}, filled: {filled}, remaining: {remaining}, avgFillPrice: {avgFillPrice}, permId: {permId}, parentId: {parentId}, lastFillPrice: {lastFillPrice}, clientId: {clientId}, whyHeld: {whyHeld}, mktCapPrice: {mktCapPrice}\n")
+
+    def openOrder(self, orderId, contract, order, orderState):
+        print(f"OpenOrder, orderId: {orderId}, contract: {contract}, order: {order}, orderState: {orderState}")
 
     def execDetails(self, reqId, contract, execution):
         print(f"ExecDetails:, reqId: {reqId}, contract: {contract}, execution: {execution}\n")
