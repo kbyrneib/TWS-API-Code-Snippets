00037ea77b34a1584c344d45367a2cb79b1b75d0
Keelan Byrne <kbyrne@interactivebrokers.co.uk>
2025-06-13 14:31:45 +0100

    add market to limit order

---
+++ Python/Orders/market_to_limit.py
@@ -0,0 +1,58 @@
+from ibapi.client import *
+from ibapi.wrapper import *
+
+class TestApp(EClient, EWrapper):
+    def __init__(self):
+        EClient.__init__(self, wrapper=self)
+
+    def nextValidId(self, orderId):
+        print(f"Next valid order ID is {orderId}")
+        self.orderId = orderId
+        self.start()
+
+    def nextId(self):
+        self.orderId += 1
+        return self.orderId
+
+    def start(self):
+        # Build Contract
+        contract = Contract()
+        contract.conId = 265598 # AAPL
+        contract.exchange = "SMART"
+        
+        # Order Type: Market To Limit Order
+        # Explanation: Market order to execute at the current best market price. 
+        # If only partially filled, remainder of the order canceled and re-submitted as a limit order with the 
+        # limit price equal to the price at which the filled portion of the order executed.
+        # General Link: https://www.interactivebrokers.com/en/trading/ordertypes.php?m=markettoLimitModal
+        # API Link: https://www.interactivebrokers.com/campus/ibkr-api-page/order-types/#market-to-limit-order
+        # Building the Order with required attributes
+        order = Order()
+        order.action = "BUY" # Identifies the side, BUY in this case
+        order.orderType = "MTL" # The order type (obviously)
+        order.totalQuantity = 1 # The number of positions being bought/sold (Qty in TWS GUI)
+        
+        # Place Order
+        self.placeOrder(self.nextId(), contract, order)
+
+    def orderStatus(self, orderId, status, filled, remaining, avgFillPrice, permId, parentId, lastFillPrice, clientId, whyHeld, mktCapPrice):
+        print(f"OrderStatus:, orderId: {orderId}, status: {status}, filled: {filled}, remaining: {remaining}, avgFillPrice: {avgFillPrice}, permId: {permId}, parentId: {parentId}, lastFillPrice: {lastFillPrice}, clientId: {clientId}, whyHeld: {whyHeld}, mktCapPrice: {mktCapPrice}\n")
+
+    def openOrder(self, orderId, contract, order, orderState):
+        print(f"OpenOrder, orderId: {orderId}, contract: {contract}, order: {order}, orderState: {orderState}")
+
+    def execDetails(self, reqId, contract, execution):
+        print(f"ExecDetails:, reqId: {reqId}, contract: {contract}, execution: {execution}\n")
+
+    def error(self, reqId, errorTime, errorCode, errorString, advancedOrderRejectJson=""):
+        print(errorCode, errorString)
+
+# Required args for connectivity
+host = "localhost"
+port = 7497
+clientId = 0
+
+# Creating, connecting and running app
+app = TestApp()
+app.connect(host, port, clientId)
+app.run()
