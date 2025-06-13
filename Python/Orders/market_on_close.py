dd67a30c49e3392d97be01cfc50af8f6c80d9d2a
Keelan Byrne <kbyrne@interactivebrokers.co.uk>
2025-06-13 14:18:21 +0100

    add market on close order

---
+++ Python/Orders/market_on_close.py
@@ -0,0 +1,56 @@
+from ibapi.client import *
+from ibapi.wrapper import *
+
+class TestApp(EClient, EWrapper):
+    def __init__(self):
+        EClient.__init__(self, wrapper=self)
+
+    def nextValidId(self, orderId):
+        print(f"Next valid order ID is {orderId}")
+        self.orderId = orderId
+        self.start()
+
+    def nextId(self):
+        self.orderId += 1
+        return self.orderId
+
+    def start(self):
+        # Build Contract
+        contract = Contract()
+        contract.conId = 265598 # AAPL
+        contract.exchange = "SMART"
+        
+        # Order Type: Market On Close Order
+        # Explanation: Market order that is submitted to execute as close to the closing price as possible
+        # General Link: https://www.interactivebrokers.com/en/trading/ordertypes.php?m=marketCloseModal
+        # API Link: https://www.interactivebrokers.com/campus/ibkr-api-page/order-types/#marker-on-close-order
+        # Building the Order with required attributes
+        order = Order()
+        order.action = "BUY" # Identifies the side, BUY in this case
+        order.orderType = "MOC" # The order type (obviously)
+        order.totalQuantity = 1 # The number of positions being bought/sold (Qty in TWS GUI)
+        
+        # Place Order
+        self.placeOrder(self.nextId(), contract, order)
+
+    def orderStatus(self, orderId, status, filled, remaining, avgFillPrice, permId, parentId, lastFillPrice, clientId, whyHeld, mktCapPrice):
+        print(f"OrderStatus:, orderId: {orderId}, status: {status}, filled: {filled}, remaining: {remaining}, avgFillPrice: {avgFillPrice}, permId: {permId}, parentId: {parentId}, lastFillPrice: {lastFillPrice}, clientId: {clientId}, whyHeld: {whyHeld}, mktCapPrice: {mktCapPrice}\n")
+
+    def openOrder(self, orderId, contract, order, orderState):
+        print(f"OpenOrder, orderId: {orderId}, contract: {contract}, order: {order}, orderState: {orderState}")
+
+    def execDetails(self, reqId, contract, execution):
+        print(f"ExecDetails:, reqId: {reqId}, contract: {contract}, execution: {execution}\n")
+
+    def error(self, reqId, errorTime, errorCode, errorString, advancedOrderRejectJson=""):
+        print(errorCode, errorString)
+
+# Required args for connectivity
+host = "localhost"
+port = 7497
+clientId = 0
+
+# Creating, connecting and running app
+app = TestApp()
+app.connect(host, port, clientId)
+app.run()
